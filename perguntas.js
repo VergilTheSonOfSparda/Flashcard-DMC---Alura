6462e0fbd3a41a3960a1339328bc98ace7f5a1c7
Eric <00001121081320sp@al.educacao.sp.gov.br>
2024-11-17 19:15:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Dante',
+    'Quem é Dante Sparda?',
+    'Filho de Sparda, Dante é um caçador de demônios e tem uma franquia de caça de demônios'
+)
+
+criaCartao(
+    'Vergil',
+    'Quem é Vergil Sparda?',
+    'Primeiro filho de Sparda, Vergil é irmão mais velho de Dante, e sempre procura por mais poder'
+)
+
+criaCartao(
+    'Nero',
+    'Quem é Nero?',
+    'Filho de Vergil, Nero é um dos híbridos mais poderosos. Ele trabalha junto a Dante em sua franquia'
+)
+
+criaCartao(
+    'V',
+    'Quem é V?',
+    'V é a parte humana de Vergil, ele procura Dante e Nero para se juntar de novo ao seu lado demônio, Urizen'
+)
